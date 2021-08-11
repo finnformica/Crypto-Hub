b0a2fcfd018d3ebcb264773436e333d6d3e9c746
finnformica <finnformica@gmail.com>
2021-08-11 18:46:25 +0100

    add user authentification

--- cryptohub/routes.py
+++ cryptohub/routes.py
@@ -1,34 +1,57 @@
-from flask import render_template, url_for, flash, redirect
-from cryptohub import app
+from flask import render_template, url_for, flash, redirect, request
+from cryptohub import app, db, bcrypt
 from cryptohub.forms import RegistrationForm, LoginForm, SpotTradeForm, PerpetualTradeForm
 from cryptohub.models import User, Spot, Perpetual
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('account created')
-        user = User(username=form.username.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
-        return redirect(url_for('home'))
+        db.session.commit()
+        flash('Your account has been created. You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash('You have been logged in', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
 
     return render_template('login.html', form=form)
 
-@app.route('/spot')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/spot', methods=['GET', 'POST'])
+@login_required
 def spot():
     form = SpotTradeForm()
     if form.validate_on_submit():
-        flash(f'Spot trade logged.', 'success')
+        flash('Spot trade logged.', 'success')
         spot_trade = Spot(
                         date=form.date.data,
                         ticker1=form.ticker1.data,
@@ -43,7 +66,8 @@ def spot():
         # return redirect(url_for('spot'))
     return render_template('spot.html', form=form)
 
-@app.route('/perpetual')
+@app.route('/perpetual', methods=['GET', 'POST'])
+@login_required
 def perpetual():
     form = PerpetualTradeForm()
     if form.validate_on_submit():
@@ -65,9 +89,11 @@ def perpetual():
     return render_template("perpetual.html", form=form)
 
 @app.route('/charts')
+
 def charts():
     return render_template('charts.html')
 
 @app.route('/settings')
+@login_required
 def settings():
     return render_template('settings.html')
